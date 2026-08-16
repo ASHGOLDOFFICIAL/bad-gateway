31eb4ce03827bd759dd659abdc2522b08dd98a3e
Andrey Shaat <104313094+ASHGOLDOFFICIAL@users.noreply.github.com>
2026-08-16 18:29:53 +0500

    model: add temperature system
    Add temperature_system, which drifts each entity's Temperature toward
    ambient via Newtonian cooling and applies any pending HeatDelta.
    
    Signed-off-by: Andrey Shaat <104313094+ASHGOLDOFFICIAL@users.noreply.github.com>

--- crates/model/src/component/physics.rs
+++ crates/model/src/component/physics.rs
@@ -1,8 +1,10 @@
-use physics::{Angle, ForceMagnitude};
-
 mod shape;
 
+pub use physics::Temperature;
 pub use shape::*;
+
+use super::utils::impl_delta_ops;
+use physics::{Angle, Energy, ForceMagnitude, ops::Delta};
 
 /// This object can change position.
 #[derive(Clone, Copy, Debug, Default, PartialEq)]
@@ -11,6 +13,13 @@ pub struct Moveable;
 /// Direction this entity move along.
 #[derive(Clone, Copy, Debug, Default, PartialEq, derive_more::From, derive_more::Into)]
 pub struct Direction(Angle);
+
+/// Used to add heat to object to be later transformed into temperature.
+#[must_use]
+#[derive(Clone, Copy, Debug, Default, PartialEq, derive_more::From, derive_more::Into)]
+pub struct HeatDelta(Delta<Energy>);
+
+impl_delta_ops!(HeatDelta => Energy);
 
 /// Rated thrust of a reaction drive.
 #[must_use]
--- crates/model/src/system/physics.rs
+++ crates/model/src/system/physics.rs
@@ -1,5 +1,7 @@
 mod collision;
 mod movement;
+mod temperature;
 
 pub use collision::*;
 pub use movement::*;
+pub use temperature::*;
